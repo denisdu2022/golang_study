a5c6f3df5d6bba5edd62a51569d9f1f79f9cd398
denisdu <denisdu@aliyun.com>
2022-07-21 17:41:56 +0800

    update

---
+++ src/gocode/oldboy09/day02/main/main.go
@@ -0,0 +1,50 @@
+package main
+
+func main() {
+	//十进制转换
+	//var a int = 10
+	//fmt.Printf("%d \n", a) //10   占位符%d表示十进制
+	//fmt.Printf("%b \n", a) //1010   占位符%b表示二进制
+	//fmt.Printf("%o \n", a) //12   占位符%o表示八进制
+	//fmt.Printf("%x \n", a) //a   占位符%x表示十六进制
+
+	//八进制转换
+	//var b int = 020
+	//fmt.Printf("%o \n", b) //20
+	//fmt.Printf("%d \n", b) //16
+	//fmt.Printf("%x \n", b) //10
+	//fmt.Printf("%b \n", b) //10000
+
+	//十六进制转换
+	//var c = 0x12
+	//fmt.Printf("%d \n", c) //18
+	//fmt.Printf("%o \n", c) //22
+	//fmt.Printf("%x \n", c) //12
+	//fmt.Printf("%b \n", c) //10010
+
+	//var f1 float32 = 3.14
+	//fmt.Println(f1, reflect.TypeOf(f1))
+	//
+	//var f2 float64 = 3.14
+	//fmt.Println(f2, reflect.TypeOf(f2))
+	//
+	//var f3 = 3.14
+	//fmt.Println(f3, reflect.TypeOf(f3)) //根据当前平台是64
+
+	//var f1 = 2e10 //即使是整数用科学计数表示也是浮点型
+	//fmt.Println(f1, reflect.TypeOf(f1))
+	//
+	//var f2 = 2e-2
+	//fmt.Println(f2, reflect.TypeOf(f2))
+
+	//fmt.Println(1 == 1)
+	//fmt.Println(2 < 1)
+	//fmt.Println(5 > 3)
+	//b := 3 > 1
+	//fmt.Println(b, reflect.TypeOf(b))
+
+	//fmt.Println('a')                //单引号''是字符
+	//fmt.Println("heelo world")      //字符串
+	//fmt.Println("hello" + "world!") //字符串拼接
+
+}
