2bcd9152dc0d16d0b0008fd66737c6d2388757dd
denisdu <denisdu@aliyun.com>
2022-07-22 16:44:57 +0800

    add codefile

--- src/gocode/oldboy09/day02/main/main.go
+++ src/gocode/oldboy09/day02/main/main.go
@@ -47,4 +47,104 @@ func main() {
 	//fmt.Println("heelo world")      //字符串
 	//fmt.Println("hello" + "world!") //字符串拼接
 
+	//var s = "hello world reboot"
+	//fmt.Println(s)
+	//
+	////1.索引取值 slice[index]
+	//a := s[2]
+	//fmt.Println(string(a))
+	//
+	////2.切片取值slice[start:end] ,取出的元素数量为:结束位置 -开始位置;
+	//b1 := s[3:6] //顾头不顾尾
+	//fmt.Println(b1)
+	//b2 := s[0:] //当缺省结束位置时,表示从开始位置到整个连续区域末尾
+	//fmt.Println(b2)
+	//b3 := s[:8] //当缺省开始位置时,表示从连续区域开头到结束位置
+	//fmt.Println(b3)
+	//
+	////3.字符串拼接
+	//var s1 = "hello"
+	//var s2 = "world reboot"
+	//var s3 = s1 + s2 //拼接成一个新的字符串
+	//fmt.Println(s3)
+
+	/*	var s1 = "hi reboot\nhi world" //换行符
+		fmt.Println(s1)
+		var s2 = "my is \"daerwen\"" //转义特殊符号
+		fmt.Println(s2)
+		var s3 = "D:\\new\\golang\\go.exe"
+		fmt.Println(s3)*/
+
+	//多行字符串
+	//s1 := `春日
+	//胜日寻芳泗水滨
+	//无边光景一时新
+	//等闲识得东风面
+	//万紫千红总是春
+	//`
+	//fmt.Println(s1)
+
+	////求字符串长度
+	//s := "daerwen,bijiasuo,jialiluo"
+	//fmt.Println(len(s))
+	//
+	////全部转大写
+	//s1 := strings.ToUpper("Derwen")
+	////全部转小写
+	//s2 := strings.ToLower("BjiSuo")
+	//
+	//fmt.Println("s1 转换后是:", s1)
+	//fmt.Println("s2 转换后是:", s2)
+	//
+	////去除字符串两端匹配的内容(比如空格)
+	//s3 := strings.Trim("    Daerwen   ", " ")
+	//fmt.Println(s3, len(s3))
+	//
+	////将字符串分割成数组
+	//var ret = strings.Split(s, ",") //以,分割
+	//fmt.Println(ret, reflect.TypeOf(ret))
+	//
+	////将数组拼接成字符串
+	//var pjret = strings.Join(ret, "~") //以~拼接
+	//fmt.Println(pjret, reflect.TypeOf(pjret))
+	//
+	////以...开头,以...结尾
+	//var index = strings.Index(s, "daerwen")
+	//fmt.Println(index, reflect.TypeOf(index))
+	////匹配到返回0,未匹配到返回-1
+	//var index2 = strings.LastIndex(s, "reboot")
+	//fmt.Println(index2, reflect.TypeOf(index2))
+	//
+	////前缀判断,后缀判断,实现的依赖就是strings.Index 类型bool值
+	//fmt.Println(strings.HasPrefix(s, "da"))
+	//fmt.Println(strings.HasSuffix(s, "la"))
+	//
+	////包含  类型bool值
+	//fmt.Println(strings.Contains(s, "a"))
+	//fmt.Println(strings.Contains(s, "1"))
+
+	//var a int8
+	//a = 100
+	//fmt.Println(reflect.TypeOf(int64(a)))
+	//fmt.Println(reflect.TypeOf(float64(a)))
+	////字节转换为字符,不能直接转为字符串
+	//fmt.Println(string(a), reflect.TypeOf(string(a)))
+	//
+	////数字与字符串之间的转换需要借助strconv库
+	//
+	//x := strconv.Itoa(112)
+	//fmt.Println(x, reflect.TypeOf(x))
+	//y, _ := strconv.Atoi("1200")
+	//fmt.Println(y, reflect.TypeOf(y))
+	//
+	////浮点型字符串转为浮点型
+	//
+	//z, _ := strconv.ParseFloat("3.14", 64)
+	//fmt.Println(z, reflect.TypeOf(z))
+
+	//a := 1
+	//a++ //注意:不能写成++a 或 --a 必须放在右边使用
+	//
+	//fmt.Println(a) //a = 2
+
 }
---
+++ src/gocode/oldboy09/lian01/main.go
@@ -0,0 +1,30 @@
+package main
+
+func main() {
+	//整形 int
+	//var x = 100
+	//fmt.Println(x, reflect.TypeOf(x)) //typeof 打印数据类型 //默认int类型
+
+	//overflows
+	//var y int8
+	//y = 160
+	//fmt.Println(y)
+
+	////布尔类型: true false
+	//var b bool
+	//b = 2 > 1 //赋值存储以供后续使用
+	//fmt.Println(b)
+
+	////索引  字符串[索引]
+	//s := "awellcome to golang!"
+	//fmt.Println(s[4])         //默认是二进制的值
+	//fmt.Println(string(s[4])) //强转为字符串
+
+	////切片 取连续的字符串 字符串[开始索引:结束索引]
+	//fmt.Println(s[1:9]) //取出Wellcome
+
+	//转义符
+	//功能1: 赋予某些普通符号以特殊功能
+	//功能2: 取消某些特殊符号的特殊功能
+
+}
