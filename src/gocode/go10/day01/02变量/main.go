5a2526087fdfbef9068951177ced92ed490e2ec1
denisdu <denisdu@aliyun.com>
2022-07-24 21:08:11 +0800

    add newcodefile

---
+++ src/gocode/go10/day01/02变量/main.go
@@ -0,0 +1,28 @@
+package main
+
+func main() {
+	/*	//先声明在赋值
+		var age int
+		fmt.Println(age)
+		//变量赋值
+		age = 22
+		fmt.Println(age)
+		age = 30
+		fmt.Println(age)
+
+		//声明和赋值写在一行
+		//var name string = "jieke"
+		var name = "jieke"
+		fmt.Println(name)
+
+		//声明多个变量
+		var (
+			a int
+			b string
+			c bool
+		)
+		fmt.Println(a) //0
+		fmt.Println(b) //""
+		fmt.Println(c) //false*/
+
+}
