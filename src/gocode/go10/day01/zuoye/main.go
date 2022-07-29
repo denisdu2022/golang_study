7864cf97dc55ee4871ab851b26c254b84d80af22
denisdu <denisdu@aliyun.com>
2022-07-29 15:40:44 +0800

    update day01 zuoye code file

---
+++ src/gocode/go10/day01/zuoye/main.go
@@ -0,0 +1,41 @@
+package main
+
+func main() {
+
+	//var a uint8
+	//a = 255
+	//fmt.Println("占用字节数: ", unsafe.Sizeof(a))
+	//fmt.Println(a, reflect.TypeOf(a), &a)
+
+	//var _ xyz = 10
+	//fmt.Println(_ xyz)
+
+	//var 2a = 10
+	//fmt.Println(2a)
+
+	//var $abc  =20
+	//name := "yuan"
+	//fmt.Println(name)
+
+	//s := "hello golang"
+	//fmt.Println(s[6:])
+	//fmt.Println(s[6:12])
+
+	//city := "北京 上海 广州 深圳"
+	//cityarr := strings.Split(city, " ")
+	//fmt.Println(strings.Join(cityarr, ","))
+
+	//s := "mysql ... -u root -p 123"
+	////s1 := strings.Contains(s, "yuan") && strings.Contains(s, "123")
+	//sNmae := strings.Contains(s, "yuan")
+	//sPwd := strings.Contains(s, "123")
+	//fmt.Println("是否包含用户名(yuan): ", sNmae, "是否包含密码(123): ", sPwd)
+
+	//fmt.Println("*****请输入你的用户名*******")
+	//var name string
+	//fmt.Scan(&name)
+	//fmt.Println(name)
+	//name1 := strings.HasPrefix(name, "a") || strings.HasPrefix(name, "A")
+	//fmt.Println("判断该名字是否以小写a或者大写A开头，如果是打印true，否则打印false: ", name1)
+
+}
