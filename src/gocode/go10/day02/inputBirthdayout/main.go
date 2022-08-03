c251a60e86ad3f07e520d6f235713a3f615d28cc
denisdu <denisdu@aliyun.com>
2022-08-03 17:11:57 +0800

    update

---
+++ src/gocode/go10/day02/inputBirthdayout/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+func main() {
+
+	var birth string
+	fmt.Println("请输入您的出生日期,格式为1997-05-23: ")
+	//接收用户输入的生日
+	fmt.Scan(&birth)
+	//fmt.Println(birth)
+
+	// 以-分割输入的字符串
+	nBirth := strings.Split(birth, "-")
+	//newBirth := strings.Join(nBirth, " ")
+	//fmt.Println(nBirth, newBirth) //nBirth 分割后是[1992 23 22]
+	//fmt.Println(reflect.TypeOf(nBirth), reflect.TypeOf(newBirth)) //分割后的类型是切片 []string  ,拼接是字符串 string
+
+	//定义两个变量,分别取月份和日期
+	mon := nBirth[1]
+	day := nBirth[2]
+	//拼接月份和日期
+	data := mon + "." + day
+	fmt.Println(data, reflect.TypeOf(data)) // 02.12 string
+
+	//month := nBirth[1]
+	//day := nBirth[2]
+	//var data = month + "." + day
+	//fmt.Println(data)
+
+	//强转为float类型
+	newdata, _ := strconv.ParseFloat(data, 64)
+	fmt.Println(newdata, reflect.TypeOf(newdata)) //2.12 float64
+
+	if newdata >= 3.21 && newdata <= 4.19 {
+		fmt.Println("你是白羊座,火象星座,幸运数字6,7，幸运颜色:鲜红色，血红色.")
+	} else if newdata >= 4.20 && newdata <= 5.20 {
+		fmt.Println("你是金牛座,土象星座,幸运数字1,9，幸运颜色:紫色.")
+	} else if newdata >= 5.21 && newdata <= 6.20 {
+		fmt.Println("你是双子座,风象星座,幸运数字3,5，7,幸运颜色:淡粉色.")
+	} else if newdata >= 6.21 && newdata <= 7.22 {
+		fmt.Println("你是巨蟹座,水象星座,幸运数字8,3，幸运颜色:大地色.")
+	} else if newdata >= 7.23 && newdata <= 8.22 {
+		fmt.Println("你是狮子座,火象星座,幸运数字5,9，幸运颜色:褐色,深红色.")
+	} else if newdata >= 8.23 && newdata <= 9.22 {
+		fmt.Println("你是处女座,土象星座,幸运数字4,8，幸运颜色:黄色.")
+	} else if newdata >= 9.23 && newdata <= 10.22 {
+		fmt.Println("你是天秤座,风象星座,幸运数字6,9，幸运颜色:橙色.")
+	} else if newdata >= 10.23 && newdata <= 11.21 {
+		fmt.Println("你是天蝎座,水象星座,幸运数字3,5，幸运颜色:暗灰色,深红色.")
+	} else if newdata >= 11.22 && newdata <= 12.21 {
+		fmt.Println("你是射手座,火象星座,幸运数字9，幸运颜色:黑色,白色,灰色.")
+	} else if newdata >= 12.22 && newdata <= 12.31 || newdata >= 1.1 && newdata <= 1.19 {
+		//上边的表达式,只是是按照提示的格式输入就可以判断摩羯座的生日范围
+		fmt.Println("你是摩羯座,土象星座,幸运数字3,7，幸运颜色:深红色.")
+	} else if newdata >= 1.20 && newdata <= 2.18 {
+		fmt.Println("你是水瓶座,风象星座,幸运数字4,8，幸运颜色:金黄色.")
+	} else if newdata >= 2.19 && newdata <= 3.20 {
+		fmt.Println("你是双鱼座,水象星座,幸运数字5,8，幸运颜色:紫色.")
+	} else {
+		fmt.Println("你输入的数值非法!!!")
+	}
+}
