3f6e3384895631928d56c586d7b12ce5bfe61397
denisdu <denisdu@aliyun.com>
2022-08-05 22:11:30 +0800

    update day02 code file

--- src/gocode/go10/day02/03 循环/main.go
+++ src/gocode/go10/day02/03 循环/main.go
@@ -4,12 +4,47 @@ import "fmt"
 
 func main() {
 
-	//打印1-100中所有的偶数
+	////for循环
+	//
+	////01 打印1到100
+	//
+	//var cont = 1
+	//for cont < 100 {
+	//	fmt.Println(cont)
+	//	cont++
+	//}
 
+	//var cont = 1
+	//for cont < 100 {
+	//	cont++
+	//	fmt.Println(cont)
+	//
+	//}
+	//
+	//for i := 1; i <= 100; i++ {
+	//	fmt.Println(i)
+	//}
+
+	////打印100-1
+	//var i = 100
+	//for i > 0 {
+	//	i--
+	//	fmt.Println(i)
+	//}
+
+	////打印1-100中所有的偶数
+	//
+	//for i := 1; i <= 100; i++ {
+	//	if i%2 == 0 {
+	//		fmt.Println(i)
+	//	}
+	//}
+
+	//计算1+到100的和
+	var s = 1
 	for i := 1; i <= 100; i++ {
-		if i%2 == 0 {
-			fmt.Println(i)
-		}
+		s += i
+		fmt.Println(s)
 	}
 
 }
