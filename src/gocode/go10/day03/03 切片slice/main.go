8dacc039db5a692794aec510b1279088895f933a
denisdu <denisdu@aliyun.com>
2022-08-09 03:15:52 +0800

    update

--- src/gocode/go10/day03/03 切片slice/main.go
+++ src/gocode/go10/day03/03 切片slice/main.go
@@ -47,4 +47,57 @@ func main() {
 	//fmt.Println(a) //[100 2 3]
 	//fmt.Println(b) //[100 2 3]
 
+	////type Slice struct {
+	////	Data uintptr //指针,指向底层数组中切片指定的开始位置
+	////	Len  int     //长度,即切片的长度
+	////	Cap  int     //最大长度(容量),也就是切片开始位置到数组的最后位置的长度
+	////}
+	//
+	////示例
+	//slice := []int{1, 2, 3, 4, 5}
+	//newSlice := slice[1:3]
+	//
+	//fmt.Println(slice)         //[1 2 3 4 5]
+	//fmt.Println(len(slice))    //5
+	//fmt.Println(cap(slice))    //5
+	//fmt.Println(newSlice)      //[2 3]
+	//fmt.Println(len(newSlice)) //2
+	//fmt.Println(cap(newSlice)) //4
+	//
+	//fmt.Println(&slice)    //&[1 2 3 4 5]
+	//fmt.Println(&newSlice) //&[2 3]
+	//
+	////地址打印
+	////a 记录的顺序表的表头,即数组指针 : 数组指针 长度 容量,所以打印a地址即打印a第一个表头地址
+	//fmt.Printf("%p\n", slice)     //0xc00000a330
+	//fmt.Printf("%p\n", &slice)    //0xc000004078
+	//fmt.Printf("%p\n", &slice[0]) //0xc00000a330
+	//fmt.Printf("%p\n", &slice[1]) //0xc00000a338
+	//fmt.Printf("%p\n", &slice[2]) //0xc00000a340
+	//fmt.Printf("%p\n", &slice[3]) //0xc00000a348
+	//fmt.Printf("%p\n", &slice[4]) //0xc00000a350
+
+	////练习
+	//var a = [...]int{1, 2, 3, 4, 5, 6}
+	//fmt.Println(a, reflect.TypeOf(a)) //[1 2 3 4 5 6] [6]int
+	//
+	//a1 := a[0:3] //[1 2 3]   3    6
+	//a2 := a[0:5] //[1 2 3 4 5 ] 5 6
+	//a3 := a[1:5] //[2 3 4 5 ] 4  5
+	//fmt.Println(a3, reflect.TypeOf(a3))
+	//a4 := a[1:]   //[2 3 4 5 6]  5  5
+	//a5 := a[:]    //[1 2 3 4 5 6]  6 6
+	//a6 := a3[1:2] //[3] 1  3
+	//fmt.Println(a6, reflect.TypeOf(a6))
+	//
+	//fmt.Printf("a1的长度%d,容量%d\n", len(a1), cap(a1))
+	//fmt.Printf("a2的长度%d,容量%d\n", len(a2), cap(a2))
+	//fmt.Printf("a3的长度%d,容量%d\n", len(a3), cap(a3))
+	//fmt.Printf("a4的长度%d,容量%d\n", len(a4), cap(a4))
+	//fmt.Printf("a5的长度%d,容量%d\n", len(a5), cap(a5))
+	//fmt.Printf("a6的长度%d,容量%d\n", len(a6), cap(a6))
+
+	//var names = []string{"张三", "李四", "王五"}
+	//fmt.Println(names, reflect.TypeOf(names)) //[张三 李四 王五] []string
+
 }
