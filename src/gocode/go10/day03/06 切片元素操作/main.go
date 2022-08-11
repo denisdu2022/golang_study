ff8d0dd19f3dfcd7b452d661c866c481359b367d
denisdu <denisdu@aliyun.com>
2022-08-11 09:37:54 +0800

    update day03 code file

---
+++ src/gocode/go10/day03/06 切片元素操作/main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+	////开头添加元素
+	//var a = []int{1, 2, 3}
+	//fmt.Println(a, len(a), cap(a)) //[1 2 3] 3 3
+	//fmt.Printf("%p\n", &a)
+	//a = append([]int{0}, a...)     //在开头添加1个元素
+	//fmt.Println(a, len(a), cap(a)) //[0 1 2 3]  4 4
+	//fmt.Printf("%p\n", &a)
+	//a = append([]int{-3, -2, -1}, a...) //在开头添加一个切片
+	//fmt.Printf("%p\n", &a)
+	//fmt.Println(a, len(a), cap(a)) //[-3 -2 -1 0 1 2 3]  7  8
+
+	//任意位置插入元素
+	//var a []int
+	//a = append([]int{x},a[i:]...)//在第i个位置插入x
+
+}
