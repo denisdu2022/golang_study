d02570897a6c8e705e64ef038df5b8ccfefb66cc
denisdu <denisdu@aliyun.com>
2022-08-22 09:31:00 +0800

    update day05 code file

--- src/gocode/go10/day04/zuoye01/main.go
+++ src/gocode/go10/day04/zuoye01/main.go
@@ -5,7 +5,13 @@ import (
 )
 
 func main() {
-	wel := `
+
+	var xuanZe = 0
+	var names, gender, ages, email string
+	cmsMap := []map[string]string{}
+
+	for true {
+		wel := `
 ----------------客户信息管理系统----------------
 		1, 添加客户
 		2, 查看客户
@@ -14,12 +20,7 @@ func main() {
 		5, 退出
 ----------------------------------------------
 `
-	fmt.Println(wel)
-	var xuanZe = 0
-	var names, gender, ages, email string
-	cmsMap := []map[string]string{{"sid": "", "name": "", "gender": "", "age": "", "email": ""}}
-
-	for true {
+		fmt.Println(wel)
 		fmt.Println("请输入您的选择: ")
 		fmt.Scan(&xuanZe)
 		switch xuanZe {
@@ -37,17 +38,32 @@ func main() {
 			cmsMap = append(cmsMap, map[string]string{"name": names, "gender": gender, "age": ages, "email": email})
 			//fmt.Println(cmsMap)
 		case 2:
-			fmt.Println("---------------客户列表开始-----1----------")
+			fmt.Println("---------------客户列表开始---------------")
 			for _, newCmsMap := range cmsMap {
+
+				fmt.Printf("姓名是:%-8s 性别是:%-8s 年龄是:%-8s 邮箱是:%-8s", newCmsMap["name"], newCmsMap["gender"], newCmsMap["age"], newCmsMap["email"])
+				fmt.Println()
 				//fmt.Println(newCmsMap)
-				for _, v := range newCmsMap {
-					//fmt.Println(cmsMapData, reflect.TypeOf(cmsMapData))
-					fmt.Printf("姓名是:%-8s,性别是:%8s,年龄是:%-8s,邮箱是:%-8s", v[0], v[1], v[2])
-				}
+				//for _, v := range newCmsMap {
+				//	//fmt.Println(cmsMapData, reflect.TypeOf(cmsMapData))
+				//	//fmt.Println(v)
+				//	//fmt.Printf("姓名是:%-8s,性别是:%8s,年龄是:%-8s,邮箱是:%-8s", )
+				//}
 			}
 			fmt.Println("---------------客户列表结束---------------")
 		case 3:
-			fmt.Println("")
+			fmt.Println("---------------客户修改开始---------------")
+			fmt.Println("请输入修改的客户信息:")
+			fmt.Println("请输入客户姓名:")
+			fmt.Scan(&names)
+			fmt.Println("请输入客户性别:")
+			fmt.Scan(&gender)
+			fmt.Println("请输入客户年龄:")
+			fmt.Scan(&ages)
+			fmt.Println("请输入客户邮箱:")
+			fmt.Scan(&email)
+			fmt.Println("---------------客户修改结束---------------")
+			cmsMap = append(cmsMap, map[string]string{"name": names, "gender": gender, "age": ages, "email": email})
 		case 4:
 			fmt.Println("")
 		case 5:
