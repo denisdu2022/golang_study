6dcb29ce30e251a95469515133de4e9059360f05
denisdu <denisdu@aliyun.com>
2022-11-26 23:11:58 +0800

    update day13 gin

---
+++ src/gocode/go10/day13/07 ginShouldbind/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type User struct {
+	User string `json:"user"`
+	Pwd  int    `json:"pwd"`
+}
+
+func main() {
+
+	r := gin.Default()
+
+	//ShouldBind
+	r.POST("/shouldBind/data", func(context *gin.Context) {
+		//{
+		//    "user":"luobo",
+		//    "pwd":"123",
+		//}  --->反序列化--->映射到结构体对象
+
+		//初始化结构体对象(实例化结构体对象)
+		user := User{}
+
+		err := context.ShouldBind(&user)
+		if err != nil {
+			return
+		}
+		//响应
+		context.JSON(200, gin.H{
+			//"ShouldBind": "ShouldBind data",
+			"User": user,
+		})
+	})
+
+	r.Run()
+
+}
