a9b6091e8307c4077126691eb1aff509d2ed0a9b
denisdu <denisdu@aliyun.com>
2022-12-09 23:31:04 +0800

    update day15 学生选课系统V1.0

--- src/gocode/go10/day15/03 gorm添加表记录/main.go
+++ src/gocode/go10/day15/03 gorm添加表记录/main.go
@@ -75,10 +75,11 @@ type Student struct {
 	Courses []Course `gorm:"many2many:student2course;constraint:OnDelete:CASCADE;"`
 }
 
-func DBInit() {
+func DBInit() *gorm.DB {
 	//数据库初始化
 	//数据库连接信息
-	dsn := "root:go20222023@tcp(127.0.0.1:3306)/css?charset=utf8mb4&parseTime=True&loc=Local"
+	//dsn := "root:go20222023@tcp(127.0.0.1:3306)/css?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "dev:CIAy2k0308@tcp(mysql-internet-cn-north-1-3156338edfb24ab1.rds.jdcloud.com:3306)/css?charset=utf8mb4&parseTime=True&loc=Local"
 
 	//创建日志对象
 	newLogger := logger.New(
@@ -102,11 +103,79 @@ func DBInit() {
 	db.AutoMigrate(&Course{})
 	db.AutoMigrate(&Class{})
 	db.AutoMigrate(&Student{})
+	return db
 }
 
+// 数据库初始化
+var db = DBInit()
+
 func add(ctx *gin.Context) {
+
+	//单表
+	//ORM映射 结构体对象对应表记录
+	//t1 := Teacher{BaseModel: BaseModel{Name: "luobo"}, Ton: 1001, Pwd: "luobo", Tel: "13456767890", Remark: "高级讲师"}
+	//fmt.Println(t1)
+	//fmt.Println(t1.Name)
+
+	//通过前端的内容设置数据库记录
+	//name := ctx.PostForm("name")
+	//ton, _ := strconv.Atoi(ctx.PostForm("Ton"))
+	//pwd := ctx.PostForm("pwd")
+	//tel := ctx.PostForm("tel")
+	//remark := ctx.PostForm("remark")
+	//t := Teacher{BaseModel: BaseModel{Name: name}, Ton: ton, Pwd: pwd, Tel: tel, Remark: remark}
+	//fmt.Println(t)
+
+	////添加单条记录
+	//翻译成insert语句
+	//db.Create(&t1) //传地址,&,会把插入的数据返回回来
+
+	//前端传值
+	//db.Create(&t)
+
+	//ctx.JSON(200, gin.H{
+	//	"msg": "数据库表记录添加成功!!!",
+	//	//"t1":  t1,
+	//	"t": t,
+	//})
+
+	//批量添加记录
+	//SQL语句中
+	//insert table_name values()
+	//t1 := Teacher{BaseModel: BaseModel{Name: "luobo1"}, Ton: 1001, Pwd: "luobo1", Tel: "13456767890", Remark: "高级讲师"}
+	//t2 := Teacher{BaseModel: BaseModel{Name: "luobo2"}, Ton: 1001, Pwd: "luobo2", Tel: "13456767890", Remark: "高级讲师"}
+	//t3 := Teacher{BaseModel: BaseModel{Name: "luobo3"}, Ton: 1001, Pwd: "luobo3", Tel: "13456767890", Remark: "高级讲师"}
+
+	//t1 := Teacher{BaseModel: BaseModel{Name: "luobo"}, Ton: 1001, Pwd: "luobo", Tel: "13456767890", Remark: "高级讲师"}
+	//t2 := Teacher{BaseModel: BaseModel{Name: "jack"}, Ton: 1002, Pwd: "jack", Tel: "13456763450", Remark: "中级讲师"}
+	//t3 := Teacher{BaseModel: BaseModel{Name: "hah"}, Ton: 1003, Pwd: "hah", Tel: "13678767890", Remark: "初级讲师"}
+	////创建切片并赋值
+	//teachers := []Teacher{t1, t2, t3}
+	////批量添加
+	//db.Create(&teachers)
+	//
+	//ctx.JSON(200, gin.H{
+	//	"msg": "数据库表记录添加成功!!!",
+	//	//"t1":  t1,
+	//	"t": teachers,
+	//})
+
+	////创建一对多记录
+	//c1 := Class{BaseModel: BaseModel{Name: "软件一班"}, Num: 36, TutorID: 7}
+	//db.Create(&c1)
+
+	////创建一对多记录,批量添加
+	c1 := Class{BaseModel: BaseModel{Name: "软件一班"}, Num: 36, TutorID: 7}
+	c2 := Class{BaseModel: BaseModel{Name: "软件二班"}, Num: 36, TutorID: 7}
+	c3 := Class{BaseModel: BaseModel{Name: "计算机科学与技术一班"}, Num: 38, TutorID: 8}
+	c4 := Class{BaseModel: BaseModel{Name: "计算机科学与技术2班"}, Num: 38, TutorID: 9}
+	classes := []Class{c1, c2, c3, c4}
+	db.Create(&classes)
+
 	ctx.JSON(200, gin.H{
-		"hello": "hello",
+		"msg": "数据库表记录添加成功!!!",
+		//"t1":  t1,
+		"classes": classes,
 	})
 }
 
@@ -115,12 +184,13 @@ func main() {
 	//创建路由对象
 	r := gin.Default()
 
-	//数据库初始化
-	DBInit()
+	////数据库初始化
+	//DBInit()
 
 	fmt.Println(r)
 
 	r.POST("/add", add)
 
+	//启动
 	r.Run()
 }
---
+++ src/gocode/go10/day15/04 学生选课系统V1/main.go
@@ -0,0 +1,235 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-contrib/multitemplate"
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+//公共的模型表
+
+type BaseModel struct {
+	ID         int        `gorm:"primaryKey"`
+	Name       string     `gorm:"type:varchar(32);unique;not null"`
+	CreateTime *time.Time `gorm:"autoCreateTime"` //创建时间
+	UpdateTime *time.Time `gorm:"autoCreateTime"` //更新时间
+	DeleteTime *time.Time `gorm:"autoCreateTime"` //删除时间
+}
+
+// 教师表
+
+type Teacher struct {
+	BaseModel
+	Ton    int
+	Pwd    string `gorm:"type:varchar(100);not null"`
+	Tel    string `gorm:"type:char(11);"`
+	Birth  *time.Time
+	Remark string `gorm:"type:varchar(255);"`
+}
+
+//课程表
+//Teacher 就是TeacherID
+
+type Course struct {
+	BaseModel
+	Credit int //学分
+	Period int //周期
+	//一对多
+	TeacherID int
+	//写全
+	//Teacher Teacher
+	//缩写
+	Teacher
+}
+
+//班级表
+
+type Class struct {
+	BaseModel
+	Num     int //班级人数
+	TutorID int
+	Tutor   Teacher
+}
+
+//学生表
+
+type Student struct {
+	BaseModel
+	Sno    int
+	Pwd    string `gorm:"type:varchar(100);not null"`
+	Tel    string `gorm:"type:char(11);"`
+	Gender byte   `gorm:"default:1"`
+	Birth  *time.Time
+	Remark string `gorm:"type:varchar(255);"`
+	//建立一对多的关联关系
+	ClassID int
+	//用那张表的主键去关联那个字段  外键约束
+	Class Class `gorm:"foreignKey:ClassID"`
+
+	//建立多对多的关联关系:通过创建第三张表 courses 只是orm层 结构体的成员变量,在sql层没有这个字段,会有一张多对多的关系表
+	Courses []Course `gorm:"many2many:student2course;constraint:OnDelete:CASCADE;"`
+}
+
+//数据库初始化函数
+
+// 数据库初始化
+var db *gorm.DB
+
+func DBInit() *gorm.DB {
+	//数据库初始化
+	//数据库连接信息
+	//dsn := "root:go20222023@tcp(127.0.0.1:3306)/css?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "dev:CIAy2k0308@tcp(mysql-internet-cn-north-1-3156338edfb24ab1.rds.jdcloud.com:3306)/css?charset=utf8mb4&parseTime=True&loc=Local"
+
+	//创建日志对象
+	newLogger := logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), //io writer
+		logger.Config{
+			//SlowThreshold: time.Second, //慢SQL阈值
+			LogLevel: logger.Info, //Log lever(日志级别)
+		},
+	)
+
+	//连接打开数据库 gorm.Open连接数据库  &gorm.Config{}是配置使用
+	db, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{
+		//日志配置
+		Logger: newLogger,
+	})
+	//db对象类似文件句柄,后续对数据库操作都使用db对象
+	fmt.Println(db)
+
+	//迁移模型类: 将模型类转换为SQL表
+	//db.AutoMigrate(&Teacher{})
+	db.AutoMigrate(&Course{})
+	db.AutoMigrate(&Class{})
+	db.AutoMigrate(&Student{})
+	return db
+}
+
+//// 结构体
+//
+//type Student struct {
+//	Sid    int
+//	Name   string
+//	Age    int
+//	Gender string
+//	Email  string
+//	Addr   string
+//}
+
+func index(ctx *gin.Context) {
+	ctx.HTML(200, "index.html", nil)
+}
+
+func student(ctx *gin.Context) {
+	//模拟从数据库取出的数据
+	//实例化结构体对象
+	//students := []Student{
+	//	{1000, "张三", 22, "male", "111@qq.com", "河北省承德市"}, {1001, "李四", 21, "male", "222@qq.com", "河北省石家庄"}, {1002, "王五", 23, "male", "333@qq.com", "河北省廊坊"}, {1003, "赵六", 20, "male", "555@qq.com", "河北省承德市"},
+	//}
+	//ctx.HTML(200, "student.html", gin.H{
+	//	"students": students,
+	//})
+	ctx.HTML(200, "student.html", gin.H{
+		"students": "学生管理",
+	})
+}
+
+func class(ctx *gin.Context) {
+	ctx.HTML(200, "class", nil)
+}
+func course(ctx *gin.Context) {
+	ctx.HTML(200, "course", nil)
+}
+
+func createMyRender() multitemplate.Renderer {
+	render := multitemplate.NewRenderer()
+	render.AddFromFiles("index.html", "templates/base.html", "templates/index.html")
+	render.AddFromFiles("student.html", "templates/base.html", "templates/student.html")
+	render.AddFromFiles("getStuAdd", "templates/base.html", "templates/getStuAdd.html")
+	render.AddFromFiles("class", "templates/base.html", "templates/class.html")
+	render.AddFromFiles("course", "templates/base.html", "templates/course.html")
+
+	return render
+}
+
+//添加学生
+
+//Get请求
+
+func GetStuAdd(ctx *gin.Context) {
+	ctx.HTML(200, "getStuAdd", nil)
+}
+
+//Post请求
+
+func PostStuAdd(ctx *gin.Context) {
+	//1.获取前端请求数据
+	var sid, genderInt, classID int
+	//转换为int
+	sid, _ = strconv.Atoi(ctx.PostForm("sid"))
+	name := ctx.PostForm("user")
+	pwd := ctx.PostForm("pwd")
+	tel := ctx.PostForm("tel")
+	genderInt, _ = strconv.Atoi(ctx.PostForm("gender"))
+	//强转为uint8
+	gender := uint8(genderInt)
+	//班级
+	classID, _ = strconv.Atoi(ctx.PostForm("class"))
+	remark := ctx.PostForm("remark")
+	fmt.Println(sid, name, pwd, tel, genderInt, gender, classID, remark)
+	//2.数据验证
+
+	//3.添加数据到数据库存储
+	//实例化结构体对象
+	addStudent := Student{BaseModel: BaseModel{Name: name}, Sno: sid, Pwd: pwd, Tel: tel, Gender: gender, ClassID: classID, Remark: remark}
+	//插入到数据库
+	db.Create(&addStudent)
+
+	//4.响应
+	ctx.JSON(200, gin.H{
+		"stu": addStudent,
+		"msg": "学生添加成功!!!",
+	})
+
+}
+
+func main() {
+	//获取路由对象
+	r := gin.Default()
+	////加载模版文件
+	//r.LoadHTMLGlob("templates/*")
+
+	//初始化数据库
+	DBInit()
+
+	//加载模版文件
+	r.HTMLRender = createMyRender()
+
+	//设置静态文件资源窗口
+	r.Static("/static", "./static")
+
+	//首页
+	r.GET("/", index)
+	//学生首页页面
+	r.GET("/student", student)
+	//添加学生
+	r.GET("/student/add", GetStuAdd)
+	r.POST("/student/add", PostStuAdd)
+
+	//班级页面
+	r.GET("/class", class)
+	//课程管理
+	r.GET("/course", course)
+
+	//启动
+	r.Run(":8090")
+
+}
